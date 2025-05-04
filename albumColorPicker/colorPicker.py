af0b9e2bf76d14c84354fea07dff69daefc02d4b
Evan Thomas <thomasevan1248@gmail.com>
2025-05-04 08:09:43 -0400

    Implemented colorPicker basic logic

---
+++ albumColorPicker/colorPicker.py
@@ -0,0 +1,55 @@
+import cv2 as cv
+from glob import glob
+import os
+
+
+#Load Image
+image_dir = os.path.dirname(os.path.abspath(__file__))
+imgPath = image_dir + '\\image.jpg'
+image = cv.imread(imgPath) # #we are ASSUMING the input color is going to be an RGB image - generally inputs are in RGB.
+
+if image is None:
+    FileNotFoundError()
+#TODO:  Implement tests for different images
+
+#Parse Image for Colors
+colorList = dict()
+print("Started...")
+for x in range(len(image)):
+    for y in range(len(image[0])):
+        #print(image[x][y].tolist())
+
+        cur = tuple(image[x][y].tolist())
+        #this would work BUT cur is a list. What data type can I use to store this data?
+
+        #coords = (x,y)
+        #use a hashmap! --> dictionary in python.
+        #the x and y coords will be
+        if cur not in colorList.keys():
+            colorList.update({cur: 1})
+        else:
+            colorList[cur] = colorList[cur] + 1
+print("Finished parsing!")
+
+#Sort the color list 
+sorted_items = sorted(colorList.items(), key=lambda kv: (kv[1], kv[0])) #type list
+final_list:list[tuple] = sorted_items[-5:] #the top 5 colors in the album art
+my_colors = [x[0] for x in final_list] #all the actual colors, first element in the list
+print(my_colors)
+#TODO: Customize # of exported colors with GUI
+
+
+
+#TODO: Visibly display most popular colors on screen preview
+#displayColors(final_list[0])
+
+#TODO: Export colors as png
+
+
+
+
+
+
+#optional: export png of the most common ones
+
+#make a separate file that takes an array of different colors, and then exports swatches of all the images
